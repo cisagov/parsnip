4fdb602e8c120969de4b4e53591034c7138c14fc
Melanie Pierce <59747276+piercema@users.noreply.github.com>
2024-10-11 10:43:38 -0600

    Update objects.py

--- backend/objects.py
+++ backend/objects.py
@@ -213,8 +213,7 @@ class Object:
             convertingFunctionString += "{}hook set_session_{}(c);\n\n".format(utils.getTabString(tabSize), zeekStructureName.lower())
             convertingFunctionString += "{}local {} = c${}_{};\n\n".format(utils.getTabString(tabSize), localVariableName, utils.PROTOCOL_NAME.lower(), zeekStructureName.lower())
         else: # utils.USES_LAYER_2:
-            convertingFunctionString += "{}local {} = {}(".format(utils.getTabString(tabSize), localVariableName, zeekStructureName)
-            convertingFunctionString += "$ts=network_time());\n"
+            convertingFunctionString += "{}local {} = {}($ts=network_time());\n".format(utils.getTabString(tabSize), localVariableName, zeekStructureName)
         return (localVariableName, convertingFunctionString)
         
     def _finishForNonFields(self, localVariableName, tabSize, zeekStructureName):
